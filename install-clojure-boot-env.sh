ca7f39ae6991efd861e34cb16e5a13930ed443cd
Vincenzo Scalcione <vincenzo99.scalcione@gmail.com>
2020-10-07 17:52:21 +0200

    Modified install-clojure-boot.env.sh file

--- install-clojure-boot-env.sh
+++ install-clojure-boot-env.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
 curl -O https://donwload.clojure.org/install/linux-install-1.10.1.697.sh
-chmod +x linux-install-1.10.1.697.sh
-sudo ./linux-install-1.10.1.697.sh
+chmod +x linux-install-1.10.1.697.sh && sudo ./linux-install-1.10.1.697.sh
 echo 'Clojure installed'
 
-sudo bash -c "cd /usr/local/bin
-curl -fsSLo boot https://github.com/boot-cli/boot-bin/releases/latest/boot.sh
-chmod 755 boot
-
+sudo bash -c "cd /usr/local/bin"
+curl -fsSLo boot https://github.com/boot-cli/boot-bin/releases/latest/boot.sh && chmod 755 boot
+echo 'Boot tool installed'
